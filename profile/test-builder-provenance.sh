1347b93e217ab1f9b427180f7de23adcba7469ee
Michael Vittrup Larsen <mvl.gh@network42.dk>
2025-07-05 09:02:37 +0200

    fix issue with catalog builder lookup due to duplicate matches

--- profile/test-builder-provenance.sh
+++ profile/test-builder-provenance.sh
@@ -3,8 +3,8 @@
 set -e
 
 for image in $(jq -rc '.functions[].image' catalog.json); do
-    digest=$(jq -r --arg image "$image" '.functions[] | select(.image | contains($image)) | .digest' catalog.json)
-    builder=$(jq -r --arg image "$image" '.functions[] | select(.image | contains($image)) | .builder' catalog.json)
+    digest=$(jq -r --arg image "$image" '.functions[] | select(.image==$image) | .digest' catalog.json)
+    builder=$(jq -r --arg image "$image" '.functions[] | select(.image==$image) | .builder' catalog.json)
 
     cosign verify --certificate-identity $builder@refs/heads/main --certificate-oidc-issuer https://token.actions.githubusercontent.com $image@$digest 2>&1 | head -n6
 done
